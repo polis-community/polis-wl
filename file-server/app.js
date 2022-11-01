ee8d9af45c37f4c767af8c0b9b304679edc1168a
Brendan Arnold <brendanarnold@gmail.com>
2022-11-01 21:03:18 +0000

    ISSUE: #3 file server crashes when .headersJson counterpart missing (#4)
    * Wrap reading of ,headersJson in try-catch as a temporary fix
    
    * Update changelog
    
    Co-authored-by: Brendan Arnold <brendan.arnold@policylab.gov.uk>

--- file-server/app.js
+++ file-server/app.js
@@ -9,21 +9,27 @@ const config = JSON.parse(fs.readFileSync(process.env.CONFIG_FILE || './fs_confi
 // Serve up public/ftp folder
 const serve = serveStatic(config.fileRoot, {
   'index': false,
-  'setHeaders': setHeaders,
+  'setHeaders': setHeaders
 });
 
 // Set header to force download
 function setHeaders (res, filePath) {
   //res.setHeader('Content-Disposition', contentDisposition(path));
   //
-  const configFile = fs.readFileSync(filePath + ".headersJson");
-  const headers = JSON.parse(configFile);
-  const headerNames = Object.keys(headers);
-  if (headerNames && headerNames.length) {
-    res.setHeader('Pragma', null);
-    headerNames.forEach((name) => {
-      res.setHeader(name, headers[name]);
-    });
+  // FIXME: This will crash if a file is requested where there isn't a .headersJson file counterpart - mitigate with a try-catch for now
+  // FIXME: This is blocking and will not scale well
+  try {
+    const configFile = fs.readFileSync(filePath + ".headersJson");
+    const headers = JSON.parse(configFile);
+    const headerNames = Object.keys(headers);
+    if (headerNames && headerNames.length) {
+      res.setHeader('Pragma', null);
+      headerNames.forEach((name) => {
+        res.setHeader(name, headers[name]);
+      });
+    }
+  } catch (error) {
+    console.error('Could not add the headers from ' + filePath + '.headersJson');
   }
 }
 
