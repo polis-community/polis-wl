0e74b8367967fed6f87754c323c83e71df07d1ee
Guido Berhoerster <guido+freiesoftware@berhoerster.name>
2023-11-30 09:01:54 +0000

    Use a local copy of twemoji images instead or relying on a CDN

--- client-admin/src/components/framework/custom-emoji.js
+++ client-admin/src/components/framework/custom-emoji.js
@@ -3,8 +3,9 @@ import emoji from 'react-easy-emoji'
 
 function customEmoji(input) {
   return emoji(input, {
-    baseUrl: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/',
-    size: '72x72'
+    protocol: '',
+    baseUrl: '',
+    size: '/images/twemoji/72x72'
   })
 }
 
